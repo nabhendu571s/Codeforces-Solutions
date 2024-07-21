6b7dbcd9036728acea49f3fffdb9b31b890c1d2b
Nabhendu  Jana <162037247+nabhendu571s@users.noreply.github.com>
2024-07-21 20:49:03 +0530

    A. A+B?
    https://codeforces.com/problemset/problem/1772/A

---
+++ day_14.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+void display(){
+   int n,s;
+   cin>>n>>s;
+   cout<<(n+s)<<endl;
+}
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+    display();
+  }
+  return 0;
+}
